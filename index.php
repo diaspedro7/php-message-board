ff9f6eff09666a45d801ab3da67be138d1b6eeef
Pedro <dsdias.pedro@gmail.com>
2026-02-04 23:31:13 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $message_name = $_POST['message_name'];
+    $message_text = $_POST['message_text'];
+
+    echo "Name: ". $message_name. "<br>";
+    echo "Text: ". $message_text;
+    
+
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Message Board</title>
+</head>
+<body>
+
+<h1>Message Board</h1>
+
+    <form action="index.php" method="POST">
+        <label for="message_name">Your name:</label>
+        <input type="text" id="message_name" name="message_name" required><br><br>
+
+        <label for="message_text">Your message:</label>
+        <input type="text" id="message_text" name="message_text" required><br><br>
+
+        <button type="submit">Send message</button>
+    </form>
+</body>
+</html>
