f468d0c6735e071aa7e421b118594696fe75ebbc
Pedro <dsdias.pedro@gmail.com>
2026-02-06 02:03:18 -0300

    feat: render messages on message board

--- functions.php
+++ functions.php
@@ -1,17 +1,12 @@
 <?php
 
+//file path
+const FILE_PATH = 'data/messages.json';
+
 function saveMessageInJSON($message_name, $message_text){
-    //file path
-    $file_path = 'data/messages.json';
+    
 
-
-    //get file data
-    $current_data = file_get_contents($file_path);
-
-    //convert json to array
-     $messages = json_decode($current_data, true);
-
-     $messages = $messages ?? [];
+    $messages = loadJSONData();
 
     //create new message
     $new_message = [
@@ -26,9 +21,24 @@ function saveMessageInJSON($message_name, $message_text){
     $json_data = json_encode($messages, JSON_PRETTY_PRINT);
 
     //save json
-    file_put_contents($file_path, $json_data);
+    file_put_contents(FILE_PATH, $json_data);
 
     return $new_message;
+}
+
+
+function loadJSONData(){
+
+
+    //get file data
+    $current_data = file_get_contents(FILE_PATH);
+
+    //convert json to array
+     $messages_array = json_decode($current_data, true);
+
+     $messages_array = $messages_array ?? [];
+
+     return $messages_array;
 }
 
 ?>
--- index.php
+++ index.php
@@ -2,20 +2,22 @@
 
 require 'functions.php';
 
+//load data
+$messages = loadJSONData();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $message_name = $_POST['message_name'];
     $message_text = $_POST['message_text'];
 
     $saved_message = saveMessageInJSON($message_name, $message_text);
+
+    $messages = loadJSONData();
+
+    //redirect
+    header('Location: index.php');
+    exit;
     
-    echo "Message saved successfully! <br>";
-
-    echo "Name: ". $saved_message['name']. "<br>";
-    echo "Text: ". $saved_message['message'];
-
 }
-
 
 ?>
 
@@ -26,10 +28,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Message Board</title>
+
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<div class="container">
 
-<h1>Message Board</h1>
+     <!-- LEFT SIDE -->
+     <div class="left">
+<h2>Send a message</h2>
 
     <form action="index.php" method="POST">
         <label for="message_name">Your name:</label>
@@ -40,5 +47,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         <button type="submit">Send message</button>
     </form>
+    </div>
+
+    <!-- RIGHT SIDE -->
+    <div class="right">
+        <h2>Messages</h2>
+
+        <?php if (empty($messages)): ?>
+        <p>No messages yet.</p>
+    <?php else: ?>
+        <?php foreach ($messages as $msg): ?>
+            <div class="message-card">
+                <div class="message-name">
+                    <?= htmlspecialchars($msg['name']) ?>
+                </div>
+                <div class="message-text">
+                    <?= htmlspecialchars($msg['message']) ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    </div>
+
+    
+
+</div>
+
 </body>
 </html>
